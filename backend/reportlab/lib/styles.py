37b729cd5bea160c25eeeb8e9302b6fe3807cb9e
Adat13 <david.toribio2610@gmail.com>
2026-07-14 10:54:21 -0500

    Fix: Remove mock reportlab folder to force python to load the real reportlab library

--- backend/reportlab/lib/styles.py
+++
@@ -1,6 +0,0 @@
-def getSampleStyleSheet():
-    return {'Heading1': ParagraphStyle('Heading1'), 'Normal': ParagraphStyle('Normal')}
-
-class ParagraphStyle:
-    def __init__(self, name, parent=None, **kwargs):
-        pass
